6fb0367d302eef44c362ed4614f2e24966fea056
Lukas Wölfinger <Lukas.Woelfinger@avanade.com>
2022-03-17 17:20:06 +0100

    [+] Parameters to WSL bash install script

--- docker-compose_WSL.sh
+++ docker-compose_WSL.sh
@@ -1,9 +1,15 @@
-cd /tmp
-mkdir dc-work
-cd dc-work
-curl -o docker-compose -kL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64
-chmod +x docker-compose
+if [ -n "$1" ];
+then
+    DCVERSION=$1
+    echo "Try to download docker compose version '$DCVERSION'."
+else
+    DCVERSION="v2.2.3"
+    echo "No parameter for version is provided. Default '$DCVERSION' is set."
+fi
+
+mkdir ~/dc-work-$DCVERSION
+curl -o ~/dc-work-$DCVERSION/docker-compose -kL "https://github.com/docker/compose/releases/download/$DCVERSION/docker-compose-linux-x86_64"
+chmod +x ~/dc-work-$DCVERSION/docker-compose
 mkdir -p ~/.docker/cli-plugins/
-mv docker-compose ~/.docker/cli-plugins/
-cd ..
-rmdir dc-work
+mv -f ~/dc-work-$DCVERSION/docker-compose ~/.docker/cli-plugins/
+rmdir ~/dc-work-$DCVERSION
